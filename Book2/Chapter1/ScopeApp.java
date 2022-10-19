4f29b4c132a71a1289ec0f3ae34669193e9385c5
Belinda Craill <36764613+Belindacraill@users.noreply.github.com>
2022-10-19 11:05:44 +0200

    Book2

---
+++ Book2/Chapter1/ScopeApp.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Listing2_1;
+
+/**
+ *
+ * @author belin
+ */
+public class ScopeApp {
+
+    /**
+     * @param args the command line arguments
+     */
+     static int x;
+    public static void main(String[] args)
+    {
+        x = 5;
+        System.out.println("main: x = " + x);
+        myMethod();
+    }
+    public static void myMethod()
+    {
+        int y;
+        y = 10;	
+        if (y == x + 5)	
+        {
+            int z;
+            z = 15;	
+            System.out.println("myMethod: z = " + z);
+        }	
+        System.out.println("myMethod: x = " + x);
+        System.out.println("myMethod: y = " + y);
+    }	
+}		
