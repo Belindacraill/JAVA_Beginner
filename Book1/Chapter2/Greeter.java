9b64ce5672e40395f2e10233d2222be23e9c4481
Belinda Craill <36764613+Belindacraill@users.noreply.github.com>
2022-10-19 10:58:17 +0200

    Chapter2

---
+++ Book1/Chapter2/Greeter.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Listing1_2;
+
+/**
+ *
+ * @author belin
+ */
+public class Greeter	
+{
+    public void sayHello()	
+    {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Book1/Chapter2/HelloApp2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Listing1_2;
+
+/**
+ *
+ * @author belin
+ */
+public class HelloApp2	
+{
+    public static void main(String[] args)	
+    {
+        Greeter myGreeterObject = new Greeter();	
+        myGreeterObject.sayHello();	
+    }
+}
